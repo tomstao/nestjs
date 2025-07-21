7816dda7ee37ac85ac917df9a7c904ec303a9805
Tao Su <96sutao@gmail.com>
2025-07-20 21:29:57 -0400

    Testing controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // /users route
+// decorator to define a controller
+export class UsersController {
+  /*
+
+  GET /users
+  GET /users/:id
+  POST /users
+  PATCH /users/:id
+  DELETE /users/:id
+  *  /users
+  * /users/:id
+  *
+  * */
+
+  @Get() // GET /users
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  // @Get('interns') // GET /users/interns
+  // findAllInterns() {
+  //   return [];
+  // }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // Patch /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
